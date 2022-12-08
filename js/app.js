8742d766d626b889869c25b1972f43cfcb91a4c8
Cody Kilpatrick <2489375+Codykilpatrick@users.noreply.github.com>
2022-12-08 15:35:26 -0500

    Define variables and update CER

--- js/app.js
+++ js/app.js
@@ -1,12 +1,15 @@
 /*-------------------------------- Constants --------------------------------*/
 
 
-
 /*---------------------------- Variables (state) ----------------------------*/
+let board, turn, winner, tie
 
 
 
 /*------------------------ Cached Element References ------------------------*/
+//* Plan A
+const squareEls = document.querySelector(".board")
+const messageEl = document.getElementById("message")
 
 
 
