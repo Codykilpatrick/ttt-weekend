ea53bd8146bd857458645ceb09fca137dc5c96f7
Cody Kilpatrick <2489375+Codykilpatrick@users.noreply.github.com>
2022-12-08 16:36:49 -0500

    Complete updateBoard function

--- js/app.js
+++ js/app.js
@@ -18,11 +18,30 @@ document.addEventListener('DOMContentLoaded', init)
 
 
 /*-------------------------------- Functions --------------------------------*/
-function init(){
+function init(evt){
 console.log("We loaded!");
-board =[null, null, null, null, null, null, null, null, null]
+board = [null, null, null, null, null, null, null, null, null]
 turn = 1
 winner = false
 tie = false
 render()
 }
+function render(){
+  updateBoard()
+  
+}
+function updateBoard(){
+  board.forEach(function(value, idx) {
+    square = squareEls.children[idx]
+    if (board[idx] === null){
+      square.innerHTML = "null"
+    }
+    else if (board[idx] === 1){
+      square.innerHTML = "1"
+    }
+    else if (board[idx] === -1){
+      square.innerHTML = "-1"
+    }
+    })
+};
+
